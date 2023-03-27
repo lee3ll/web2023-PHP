fa00b722ec75529213d863dcee30ee944d381772
leeeunji <lee3ll@gmail.com>
2023-03-27 17:36:39 +0900

    23.03.27

--- connect/connect.php
+++ connect/connect.php
@@ -2,12 +2,13 @@
     $host = "localhost";
     $user = "root";
     $pw = "root";
-    $db = "lee3ll";
+    $db = "sample";
     $connect = new mysqli($host, $user, $pw, $db);
     $connect -> set_charset("utf-8");
+
     if(mysqli_connect_errno()){
         echo "Database Connect False";
     } else {
-        echo "Database Connect True";
+        // echo "Database Connect True";
     }
 ?>
---
+++ sample/sample05.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://webfontworld.github.io/NexonLv1Gothic/NexonLv1Gothic.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family:"NexonLv1Gothic";
+        }
+        body {
+            padding: 20px;
+        }
+        li {
+            list-style: none;
+            margin: 4px 0;
+        }
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+        main {
+            display: flex;
+            justify-content: space-between;
+        }
+        aside {
+            width: 30%;
+            background-color: #ccc;
+        }
+        section {
+            width: 70%;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <aside>
+            <?php
+                include "aside.php";
+            ?>
+        </aside>
+        <section>
+            <?php
+                include "../connect/connect.php";
+            ?>
+        </section>
+    </main>
+</body>
+</html>
