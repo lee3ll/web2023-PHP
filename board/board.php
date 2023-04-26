ac0638a7242f65d3e2d81dfe0291776da560589c
lee3ll <125418017+lee3ll@users.noreply.github.com>
2023-04-26 18:32:53 +0900

    fg

--- board/board.php
+++ board/board.php
@@ -2,7 +2,6 @@
     include "../connect/connect.php";
     include "../connect/session.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -10,28 +9,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>게시판</title>
-
-    <?php include "../include/head.php"?>
+    <?php include "../include/head.php" ?>
 </head>
 <body class="gray">
     <?php include "../include/skip.php" ?>
-    <!-- //skip -->
-
+    <!-- skip -->
     <?php include "../include/header.php" ?>
     <!-- header -->
-
     <main id="main" class="container">
         <div class="intro__inner bmStyle">
             <picture class="intro__images">
                 <source srcset="../assets/img/intro01.png, ../assets/img/intro01@2x.png 2x, ../assets/img/intro01@3x.png 3x" />
                 <img src="../assets/img/intro01.png" alt="소개이미지">
-            </picture> 
+            </picture>
             <p class="intro__text">
                 어떤 일이라도 노력하고 즐기면 그 결과는 빛을 바란다고 생각합니다.
-                신입의 열정과 도전정신을 깊숙히 새기며 배움에 있어 겸손함을 
+                신입의 열정과 도전정신을 깊숙히 새기며 배움에 있어 겸손함을
                 유지하며 세부적인 곳까지 파고드는 개발자가 되겠습니다.
             </p>
         </div>
+        <!-- board__inner -->
         <div class="board__inner">
             <div class="board__search">
                 <div class="left">
@@ -47,8 +44,8 @@
                                 <option value="content">내용</option>
                                 <option value="name">등록자</option>
                             </select>
-                            <button type="submit" class="btnStyle3 white">검색</button>
-                            <a href="boardWrite.php" class="btnStyle3">글쓰기</a>
+                            <button type="submit" class="btnstyle3 white">검색</button>
+                            <a href="boardWrite.php" class="btnstyle3">글쓰기</a>
                         </fieldset>
                     </form>
                 </div>
@@ -59,7 +56,7 @@
                         <col style="width:5%">
                         <col>
                         <col style="width:10%">
-                        <col style="width:15%">
+                        <col style="width:15">
                         <col style="width:7%">
                     </colgroup>
                     <thead>
@@ -74,33 +71,38 @@
                     <tbody>
                         <!-- <tr>
                             <td>1</td>
-                            <td><a href="boardView.html"></a>게시판 제목</td>
-                            <td>이은지</td>
+                            <td><a href="boardView.html">게시판 제목</a></td>
+                            <td>김현빈</td>
                             <td>2023-04-24</td>
                             <td>100</td>
                         </tr> -->
 <?php
-    $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM  board b JOIN members m ON(b.memberID = m.memberID) ORDER BY boardID DESC LIMIT 20";
+    if(isset($_GET['page'])){
+        $page = (int) $_GET['page'];
+    } else {
+        $page = 1;
+    }
+    $viewNum = 10;
+    $viewLimit = ($viewNum * $page) - $viewNum;
+    //1~20  DESC LIMIT 0, 20      ---> page1 (viewNum * 1) - viewNum
+    //21~40  DESC LIMIT 20, 20    ---> page2 (viewNum * 2) - viewNum
+    //41~60  DESC LIMIT 40, 20    ---> page3 (viewNum * 3) - viewNum
+    //61~80  DESC LIMIT 60, 20    ---> page4 (viewNum * 4) - viewNum
+    $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM  board b JOIN members m ON(b.memberID = m.memberID) ORDER BY boardID DESC LIMIT {$viewLimit},{$viewNum}";
     $result = $connect -> query($sql);
-
     if($result){
         $count = $result -> num_rows;
-
         if($count > 0){
             for($i=0; $i<$count; $i++){
                 $info = $result -> fetch_array(MYSQLI_ASSOC);
-
                 echo "<tr>";
                 echo "<td>".$info['boardID']."</td>";
-                echo "<td><a href='boardView.php?boardID={$info['boardID']}'>".$info['boardTitle']."</a></td>";     //? : 속성과 속성값
+                echo "<td><a href='boardView.php?boardID={$info['boardID']}'>".$info['boardTitle']."</a></td>";
                 echo "<td>".$info['youName']."</td>";
                 echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
                 echo "<td>".$info['boardView']."</td>";
-                echo "<tr>";
-                // echo "<pre>";
-                // var_dump($info);
-                // echo "</pre>";                
-            }
+                echo "</tr>";
+            };
         }
     }
 ?>
@@ -109,7 +111,62 @@
             </div>
             <div class="board__pages">
                 <ul>
-                    <li><a href="#">처음으로</a></li>
+<?php
+    //게시글 총 갯수
+    //몇 페이지??
+    $sql = "SELECT count(boardID) FROM board";
+    $result = $connect -> query($sql);
+    $boardTotalCount = $result -> fetch_array(MYSQLI_ASSOC);
+    $boardTotalCount = $boardTotalCount['count(boardID)'];
+
+    //총 페이지 갯 수
+    $boardTotalCount = ceil($boardTotalCount/$viewNum);
+
+    // echo $boardTotalCount;
+    //1 2 3 4 5 6 [7] 8 9
+    $pageView = 5;
+    $startPage =  $page - $pageView;
+    $endPage = $page + $pageView;
+
+    //처음 페이지 초기화
+    if($startPage < 1) $startPage = 1;
+
+    //마지막 페이지 초기화
+    if($endPage >= $boardTotalCount) $endPage = $boardTotalCount;
+   
+    //처음으로
+    if($page != 1 && $page <= $boardTotalCount){
+        $prevPage = $page-1;
+        echo "<li><a href='board.php?page=1'>처음으로</a></li>";
+        echo "<li><a href='board.php?page={$prevPage}'>이전</a></li>";
+    }   
+       
+    //페이지
+    for($i=$startPage; $i<=$endPage; $i++){
+        $active = "";
+        if($i == $page) $active = "active";
+        
+        if($page <= $boardTotalCount){
+        echo "<li class='{$active}'><a href='board.php?page={$i}'>{$i}</a></li>";
+        }
+    }
+
+    //마지막으로/다음
+    if($page != $boardTotalCount && $page <= $boardTotalCount){
+        $nextPage = $page+1;
+        echo "<li><a href='board.php?page={$nextPage}'>다음</a></li>";
+        echo "<li><a href='board.php?page={$boardTotalCount}'>마지막으로</a></li>";
+    } 
+
+    //게시글이 없을 때 
+    // if($page > $boardTotalCount || $boardTotalCount == 0){
+    //     echo "<li style='display:none'><a href='board.php?page=1'>처음으로</a></li>";
+    //     echo "<li style='display:none'><a href='board.php?page={$prevPage}'>이전</a></li>";
+    //     echo "<li style='display:none'><a href='board.php?page={$nextPage}'>다음</a></li>";
+    //     echo "<li style='display:none'><a href='board.php?page={$boardTotalCount}'>마지막으로</a></li>";
+    // }
+?>
+                    <!-- <li><a href="#">처음으로</a></li>
                     <li><a href="#">이전</a></li>
                     <li class="active"><a href="#">1</a></li>
                     <li><a href="#">2</a></li>
@@ -120,15 +177,11 @@
                     <li><a href="#">6</a></li>
                     <li><a href="#">7</a></li>
                     <li><a href="#">다음</a></li>
-                    <li><a href="#">마지막으로</a></li>
+                    <li><a href="#">마지막으로</a></li> -->
                 </ul>
             </div>
         </div>
-    <!-- //intro__inner -->
-    </main>
-    <!-- main -->
-
     <?php include "../include/footer.php" ?>
-    <!-- //footer -->
+    </main>
 </body>
 </html>
--- board/boardModify.php
+++ board/boardModify.php
@@ -1,6 +1,7 @@
 <?php
     include "../connect/connect.php";
     include "../connect/session.php";
+    include "../connect/sessionCheck.php";
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>게시판</title>
+    <title>게시판 수정하기</title>
 
     <?php include "../include/head.php"?>
 </head>
@@ -26,7 +27,7 @@
                 <source srcset="../assets/img/intro01.png, ../assets/img/intro01@2x.png 2x, ../assets/img/intro01@3x.png 3x" />
                 <img src="../assets/img/intro01.png" alt="소개이미지">
             </picture> 
-            <h2>게시글 작성하기</h2>
+            <legend class="blind">게시글 작성하기</legend>
             <p class="intro__text">
             웹디자이너,웹퍼블리셔,프론트앤드 개발자를 위한 게시판입니다.
                 관련된 문의사항은 여기서 확인하세요! 
@@ -35,22 +36,38 @@
 
         <div class="board__inner">
             <div class="board__write">
-                <form action="boardWriteSave.php" name="boardWrite" method="post">
+                <form action="boardModifySave.php" name="boardWrite" method="post">
                     <fieldset>
                         <legend class="blind">게시글 작성하기</legend>
                         <div>
-                            <label for="boardTitle">제목</label>
+                        <?php
+    $boardID = $_GET['boardID'];
+
+    $sql = "SELECT boardID, boardTitle, boardContents FROM board WHERE boardID = {$boardID}";
+    $result = $connect -> query($sql);
+
+    if($result){
+        $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+        echo "<div style='display:none'><label for='boardID'>아이디</label><input type='text' id='boardID' name='boardID' class='inputStyle' value='".$info['boardID']."'></div>";
+        echo "<div><label for='boardTitle'>제목</label><input type='text' id='boardTitle' name='boardTitle' class='inputStyle' value='".$info['boardTitle']."'></div>";
+        echo "<div><label for='boardContents'>내용</label><textarea name='boardContents' id='boardContents' rows='20' class='inputStyle'>".$info['boardContents']."</textarea></div>";
+        echo "<div class='mt50'><label for='boardPass'>비밀번호</label><input type='password' id='boardPass' name='boardPass' class='inputStyle' autocomplete='off' required placeholder='글을 수정하려면 로그인 비밀번호를 입력하셔야 합니다.'></div>";
+    }
+
+?>
+                           <!--  <label for="boardTitle">제목</label>
                             <input type="text" id="board__title" name="boardTitle" class="inputStyle">
                         </div>
                         <div>
                             <label for="boardContents">내용</label>
                             <textarea name="boardContents" id="boardContents" rows="20" class="inputStyle"></textarea>
-                        </div>
+                        </div>-->
                         <button type="submit" class="btnStyle3">저장하기</button>
                     </fieldset>
                 </form>
             </div>
-         </div>
+         </div> 
     </main>
     <!-- main -->
 
